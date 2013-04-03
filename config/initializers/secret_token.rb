bcf5ab78a3b343331686cdc7a31c670ed6efffc2
bryants <bryantsatterfield@gmail.com>
2013-04-03 03:38:25 -0400

    initial commit...restarting again...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Convo::Application.config.secret_token = 'bb7f19fd02b7544e992dc5b3ce0c64aafd514631aabd019742b7f2f30a0d07d122b910e061714e790359828a5667e6e03969c30fcafc05b36784008efe541f73'
